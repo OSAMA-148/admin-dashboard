66242e76922843a5a0ed03afa19c7e0dcdb349a2
osama <om2951855@gmail.com>
2025-07-06 18:21:22 +0300

    feat: add Nivo bar chart component with data visualization
    - Updated package.json and package-lock.json to include @nivo/bar version 0.99.0
    - Created BarChart component to display a bar chart using Nivo
    - Implemented bar chart data and configuration in bar.jsx
    - Integrated responsive design and theming with MUI

--- src/page/barChart/BarChart.jsx
+++ src/page/barChart/BarChart.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { ResponsiveBar } from "@nivo/bar";
+import { Box } from "@mui/material";
+import Bar from "./bar";
+import Header from "../../components/Header";
 
 const BarChart = () => {
-  return (
-    <div>BarChart</div>
-  )
-}
+    // const theme = useTheme();
+    return (
+        <Box>
+            <Header
+                title="Bar Chart"
+                subTitle="The minimum wage in Germany, France and Spain (EUR/month)"
+            />
+            <Bar />
+        </Box>
+    );
+};
 
-export default BarChart
+export default BarChart;
